e0bdf7b8e3ccc51996d5f9401bbaba3dd704d5ef
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-06-15 18:14:42 +0200

    Update basicsyntax2.cpp

---
+++ basicsyntax2.cpp
@@ -0,0 +1,11 @@
+// basicsyntax.cpp
+// Programa que muestra Hello Workd
+
+// Incluyo las librerias a usar
+#include <iostream>     // iostream allows output the data to through script.
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
