5d4481ef4d93543efc83aefed246e419abac3605
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-06-15 18:13:49 +0200

    Update basicsyntax.cpp

---
+++ basicSyntax.cpp
@@ -0,0 +1,10 @@
+// basicsyntax.cpp
+// Programa que muestra Hello Workd
+
+// Incluyo las librerias a usar
+#include <iostream>     // iostream allows output the data to through script.
+
+int main(){
+    std:: cout<<"Hello World";
+    return 0;
+}
